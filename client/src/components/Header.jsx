969249826bb5057c38ff5c58775db36cb30c17b7
vishnaka-dasanayaka <vishnakadasanayaka@gmail.com>
2024-06-24 00:27:35 +0530

    home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,16 +4,28 @@ import name from "../assets/img/name.png";
 function Header() {
   return (
     <div>
-      <div className=" fixed flex text-[rgb(83,83,83)] w-full  items-center justify-between h-16 px-10 py-5 ">
-        <div className="flex justify-center w-1/3 h-full">
+      <div className=" fixed z-10 flex text-[rgb(83,83,83)] w-full  items-center justify-between h-16 px-10 py-5 ">
+        <div className="flex justify-start w-1/3 h-full">
           <img src={name} className="h-full" alt="name" />
         </div>
         <div className="flex items-center justify-around w-1/3">
-          <h1 className="text-xs uppercase">services</h1>
-          <h1 className="text-xs uppercase">projects</h1>
-          <h1 className="text-xs uppercase">experience</h1>
+          <h1 className="text-xs uppercase transition duration-700 ease-in-out cursor-pointer hover:bg-red-500 hover:underline">
+            services
+          </h1>
+          <h1 className="text-xs uppercase transition duration-700 ease-in-out cursor-pointer hover:underline">
+            projects
+          </h1>
+          <h1 className="text-xs uppercase transition duration-700 ease-in-out cursor-pointer hover:underline">
+            experience
+          </h1>
+          <h1 className="text-xs uppercase transition duration-700 ease-in-out cursor-pointer hover:underline">
+            blogs
+          </h1>
+          <h1 className="text-xs uppercase transition duration-700 ease-in-out cursor-pointer hover:underline">
+            contact me
+          </h1>
         </div>
-        <div className="flex justify-center w-1/3 ">
+        <div className="flex justify-end w-1/3 ">
           <h1 className="text-xs">(+94) 71 370 4691</h1>
         </div>
       </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,61 @@
-import React from "react";
-import Header from "../components/Header";
+import me from "../assets/img/me.png";
+import bg from "../assets/img/bg.jpg";
+import bg_2 from "../assets/img/bg-2.png";
+import bg_3 from "../assets/img/bg_3.png";
+import foe from "../assets/img/foe.jpeg";
 
 function Home() {
   return (
-    <div>
-      <h1>home</h1>
+    <div className="relative flex items-center justify-end w-screen h-screen">
+      <div className="z-10 flex flex-col items-start justify-between w-1/4 h-full pb-24 ml-32 pt-44 ">
+        <div className="text-5xl font-extrabold tracking-wider capitalize">
+          <h1 className="mb-5 ">Hey there,</h1>
+          <h1>I'm Vishnaka</h1>
+        </div>
+
+        <div>
+          <h1 className="text-sm text-[rgb(173,181,219)] cursor-pointer hover:text-blue-500 underline">
+            vishnakadasanayaka@gmail.com
+          </h1>
+        </div>
+        <div className="flex items-center justify-center">
+          <div>
+            <h1 className="text-4xl">2</h1>
+          </div>
+          <div className="flex flex-col items-start justify-center ml-2 text-xs uppercase">
+            <h1>years</h1>
+            <h1>experience</h1>
+          </div>
+        </div>
+      </div>
+      <div className="z-10 flex items-end justify-center w-1/2 h-full bg-transparent ">
+        <img src={me} className="object-cover w-80%  h-5/6 " alt="" />
+      </div>
+
+      <div className="z-10 flex flex-col items-end justify-between w-1/4 h-full pb-24 mr-32 pt-44 ">
+        <div className="flex flex-col items-end text-lg font-semibold tracking-wider ">
+          <h1 className="">I develop full-stack web apps</h1>
+          <h1>using MERN stack</h1>
+        </div>
+
+        <div className="flex flex-col items-center justify-center">
+          <div className="w-16 h-auto">
+            <img src={foe} alt="" />
+          </div>
+          <div className="flex flex-col items-center justify-center mt-5 ml-2 text-xs ">
+            <h1>B.Sc. Engineering (Hons) - UG</h1>
+            <h1>Faculty of Engineering</h1>
+            <h1>University of Ruhuna</h1>
+          </div>
+        </div>
+      </div>
+      <div className="absolute z-0 flex items-center justify-center w-full h-full ">
+        <img
+          src={bg_3}
+          className="object-cover w-full h-auto scale-75 opacity-50"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
